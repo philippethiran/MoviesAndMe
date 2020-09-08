4d7a0272d49501b16008ea192dbba48cf2b885d8
philippethiran <pthiran@gmail.com>
2020-09-08 14:02:11 +0200

    FINAL

--- Components/Search.js
+++ Components/Search.js
@@ -2,44 +2,46 @@
 
 import React from 'react'
 import { StyleSheet, View, TextInput, Button, Text, FlatList } from 'react-native'
-import films from '../Helpers/filmsData'
 import FilmItem from './FilmItem'
-import { getFilmsFromApiWithSearchedText } from '../TMDBApi' // import { } from ... car c'est un export nommé dans TMDBApi.js
-
-// Components/Search.js
+import { getFilmsFromApiWithSearchedText } from '../TMDBApi'
 
 class Search extends React.Component {
- constructor(props) {
+
+  constructor(props) {
     super(props)
-    this.state = { films: [] }
+    this.searchedText = "" // Initialisation de notre donnée searchedText en dehors du state
+    this.state = {
+      films: []
+    }
   }
 
-
-_loadFilms() {
-    console.log(this.state.searchedText) // Un log pour vérifier qu'on a bien le texte du TextInput
-    if (this.state.searchedText.length > 0) { // Seulement si le texte recherché n'est pas vide
-      getFilmsFromApiWithSearchedText(this.state.searchedText).then(data => {
+  _loadFilms() {
+    if (this.searchedText.length > 0) { // Seulement si le texte recherché n'est pas vide
+      getFilmsFromApiWithSearchedText(this.searchedText).then(data => {
           this.setState({ films: data.results })
       })
     }
-}
+  }
+
+  _searchTextInputChanged(text) {
+    this.searchedText = text // Modification du texte recherché à chaque saisie de texte, sans passer par le setState comme avant
+  }
+
   render() {
+    console.log("RENDER")
     return (
-
-
       <View style={styles.main_container}>
         <TextInput
-            style={styles.textinput}
-            placeholder='Titre du film'
-
-            onSubmitEditing={() => this._loadFilms()}
-          />
-
-   <FlatList
-     data={this.state.films}
-     keyExtractor={(item) => item.id.toString()}
-     renderItem={({item}) => <FilmItem film={item}/>}
-   />
+          style={styles.textinput}
+          placeholder='Titre du film'
+          onChangeText={(text) => this._searchTextInputChanged(text)}
+        />
+        <Button title='Rechercher' onPress={() => this._loadFilms()}/>
+        <FlatList
+          data={this.state.films}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({item}) => <FilmItem film={item}/>}
+        />
       </View>
     )
   }
